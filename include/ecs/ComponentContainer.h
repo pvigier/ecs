a95256c21aec3c5b2fa565b008d8ee895a063367
Pierre Vigier <pierre.vigier@ymail.com>
2019-10-04 15:32:32 +0200

    Remove useless reserve method in ComponentContainer

--- include/ecs/ComponentContainer.h
+++ include/ecs/ComponentContainer.h
@@ -11,7 +11,6 @@ struct BaseComponentContainer
 {
     virtual ~BaseComponentContainer() = default;
 
-    virtual void reserve(std::size_t size) = 0;
     virtual BaseComponent& get(ComponentId componentId) = 0;
     virtual void remove(ComponentId componentId) = 0;
 };
@@ -20,11 +19,6 @@ template<typename T>
 struct ComponentContainer : public BaseComponentContainer
 {
     ComponentSparseSet<T> components;
-
-    virtual void reserve(std::size_t size) override
-    {
-        components.reserve(size);
-    }
 
     virtual BaseComponent& get(ComponentId componentId) override
     {
